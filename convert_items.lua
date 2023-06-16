54a81ca634e680865f4dbaddbf318d57b6370c01
FellaHighOnaBoat <88798821+FellaHighOnaBoat@users.noreply.github.com>
2023-06-16 19:49:55 +0100

    upload

---
+++ convert_items.lua
@@ -0,0 +1,37 @@
+local serpent = require("serpent")
+
+function transform(item)
+    return {
+        label = item.label,
+        weight = item.weight,
+        stack = not item.unique,
+    }
+end
+
+dofile("have a file structured like this and nothing else https://i.imgur.com/eTFekcY.png and it will work.")
+
+for k, v in pairs(ItemList) do
+    ItemList[k] = transform(v)
+end
+
+local function serialize(t)
+    local kv = {}
+    for k,v in pairs(t) do
+        if type(v) == "string" then
+            v = "'" .. v .. "'"
+        elseif type(v) == "boolean" then
+            v = tostring(v)
+        end
+        table.insert(kv, k .. " = " .. v)
+    end
+    return "{ " .. table.concat(kv, ", ") .. " }"
+end
+
+local file = io.open("where ever the new file is - make sure its empty - you can copy it over to ox items.lua", "w")
+
+file:write("ItemList = {\n")
+for k, v in pairs(ItemList) do
+    file:write("['" .. k .. "'] = " .. serialize(v) .. ",\n")
+end
+file:write("}\n")
+file:close()
